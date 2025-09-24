0622edf344da8be5a0c36af2c5eb90a7d3b48f48
Decker <deckerurbano@gmail.com>
2025-09-24 09:31:07 -0300

    feat: Implement redirects in Next.js configuration for quickstart documentation; update HomePage component to display static content instead of redirecting based on language.

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,25 @@ const config: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/docs/library/quickstart",
+        permanent: true,
+      },
+      {
+        source: "/es",
+        destination: "/es/docs/library/quickstart",
+        permanent: true,
+      },
+      {
+        source: "/pt",
+        destination: "/pt/docs/library/quickstart",
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default withMDX(config);
--- src/app/[lang]/(home)/page.tsx
+++ src/app/[lang]/(home)/page.tsx
@@ -1,12 +1,7 @@
-import { redirect } from "next/navigation";
-
-interface HomePageProps {
-  params: Promise<{
-    lang: string;
-  }>;
-}
-
-export default async function HomePage(props: HomePageProps) {
-  const { lang } = await props.params;
-  return redirect(`/${lang}/docs/library/quickstart`);
+export default async function HomePage() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
 }
